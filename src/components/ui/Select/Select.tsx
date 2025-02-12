2acb1872cd5b408077c1e9ebe8b6da705f7e7fc7
Mykola Korzhykov <mr.purple2007@gmail.com>
2025-02-12 00:27:16 -0600

    Survey Calendar

--- src/components/ui/Select/Select.tsx
+++ src/components/ui/Select/Select.tsx
@@ -1,53 +1,70 @@
-import { FC, useState } from 'react'
+import { FC, useState } from "react";
 
-import SelectArrow from '@img/select/arrow.svg'
+import SelectArrow from "@img/select/arrow.svg";
 
-import styles from './Select.module.scss'
-import { toggleOverlay } from '@store/slices/overlaySlice'
-import { useAppDispatch } from '@store/hook'
+import styles from "./Select.module.scss";
+// import { toggleOverlay } from "@store/slices/overlaySlice";
+// import { useAppDispatch } from "@store/hook";
 
 type Props = {
-    list: string[]
-    value: any
-    setValue: any
-    placeholder?: string
-}
+  list: string[];
+  value: any;
+  setValue: any;
+  placeholder?: string;
+  className?: string;
+};
 
-const Select: FC<Props> = ({ list, value, setValue, placeholder = 'Choose' }) => {
-    const [isOpen, setIsOpen] = useState<boolean>(false);
+const Select: FC<Props> = ({
+  list,
+  value,
+  setValue,
+  placeholder = "Choose",
+  className = "",
+}) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
-    const dispatch = useAppDispatch()
+  // const dispatch = useAppDispatch()
 
-    const toggleSelect = () => {
-        setIsOpen(!isOpen)
-        dispatch(toggleOverlay())
-    }
+  const toggleSelect = () => {
+    setIsOpen(!isOpen);
+    // dispatch(toggleOverlay())
+  };
 
-    const selectItem = (item: string) => {
-        toggleSelect()
-        setValue(item)
-    }
+  const selectItem = (item: string) => {
+    toggleSelect();
+    setValue(item);
+  };
 
-    return (
-
-        <div className={styles.wrapper}>
-            <div className={`${styles.button} ${isOpen ? styles.buttonActive : ''}`} onClick={toggleSelect}>
-                <div className={`${styles.row} ${isOpen ? styles.rowActive : ''}`}>
-                    <p className={`${styles.value} ${isOpen ? styles.valueActive : ''}`}>{!value ? placeholder : value}</p>
-                    <div className={styles.arrow}>
-                        <SelectArrow />
-                    </div>
-                </div>
-            </div>
-            <ul className={`${styles.list} ${isOpen ? styles.listActive : ''}`}>
-                {list.filter(item => item !== value).map((item, index) => (
-                    <li className={styles.item} onClick={() => selectItem(item)} key={index}>
-                        {item}
-                    </li>
-                ))}
-            </ul>
+  return (
+    <div className={`${styles.wrapper} ${className}`}>
+      <div
+        className={`${styles.button} ${isOpen ? styles.buttonActive : ""}`}
+        onClick={toggleSelect}
+      >
+        <div className={`${styles.row} ${isOpen ? styles.rowActive : ""}`}>
+          <p className={`${styles.value} ${isOpen ? styles.valueActive : ""}`}>
+            {!value ? placeholder : value}
+          </p>
+          <div className={styles.arrow}>
+            <SelectArrow />
+          </div>
         </div>
-    )
-}
+      </div>
+      <ul className={`${styles.list} ${isOpen ? styles.listActive : ""}`}>
+        {list
+          .filter((item) => item !== value)
+          .map((item, index) => (
+            <li
+              className={styles.item}
+              onClick={() => selectItem(item)}
+              key={index}
+            >
+              {item}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
 
-export default Select
+export default Select;
